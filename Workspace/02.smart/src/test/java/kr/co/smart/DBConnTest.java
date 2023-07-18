e32449cb785356a670a17037283579c67cb22341
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-07-18 18:41:26 +0900

    ++

--- Workspace/02.smart/src/test/java/kr/co/smart/DBConnTest.java
+++ Workspace/02.smart/src/test/java/kr/co/smart/DBConnTest.java
@@ -1,16 +1,12 @@
 package kr.co.smart;
 
-import java.sql.Connection;
 import java.util.Scanner;
-
-import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
